32ae2a59480beda31ea016e85ab9e0fef0b1e7a1
fathorrosi-dev <fathorrosi-dev@proton.me>
2026-01-12 07:32:57 +0700

    chore: upgrade packages and migrate deprecated Flutter APIs

---
+++ lib/screen/styles/card_themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardThemes {
+  CardThemes._();
+
+  static final CardThemeData lightThemeData = CardThemeData(
+    color: Colors.grey.shade200,
+    elevation: 0,
+  );
+
+  static final CardThemeData darkThemeData = CardThemeData(
+    color: Colors.grey.shade900,
+    elevation: 0,
+  );
+}
